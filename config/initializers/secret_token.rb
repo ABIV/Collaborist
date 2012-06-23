ab0e9d102b368f385331ea5cbb8071fe8a67c496
Arthur Beisang <arthur@projectsite.co>
2012-06-23 09:32:00 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collaborist::Application.config.secret_token = '5075ea6d28ded581dce3b31ea6419c6d6bab27d048ab58ae7cf6a12fac3ee8d916e0be36b3e62b2581275186a61de697c9020c65ff7c97f5f660f1e10b94f0b5'
