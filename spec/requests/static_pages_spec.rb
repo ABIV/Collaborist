0af6cca2751adbab31cb6145ae5a5291236dbc41
Arthur Beisang <arthur@projectsite.co>
2012-06-25 10:21:17 -0500

    update title on home, about, and help pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Collaborist')
     end 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Collaborist | Home")
+      page.should have_selector('title', :text => "Collaborist")
+    end
+    it "should not have a custom page title" do 
+    	visit '/static_pages/home'
+    	page.should_not have_selector('title', :text => '| Home')
     end
   end
   
