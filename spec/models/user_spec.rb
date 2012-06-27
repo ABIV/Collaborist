569437027c9520a514f48e8e577fa378410a5c17
Arthur Beisang <arthur@projectsite.co>
2012-06-26 21:27:30 -0500

    Add test for mixed case email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,16 @@ describe User do
 		it { should_not be_valid }
 	end
 	
+	describe "email address with mixed case" do 
+		let(:mixed_case_email) { "Foo@ExAMPLe.Com" }
+		
+		it "should be saved as all lower_case" do 
+			@user.email = mixed_case_email
+			@user.save 
+			@user.reload.email.should == mixed_case_email.downcase 
+		end
+	end
+	
 	describe "when email format is invalid" do 
 		it "should be invalid" do 
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
