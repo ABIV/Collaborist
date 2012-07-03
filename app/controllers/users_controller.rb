99aaf5491151f9ba44aacb46f8739c1306e694d1
Arthur Beisang <arthur@projectsite.co>
2012-07-03 12:09:43 -0500

    finish sign in/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
 	def create 
 		@user = User.new(params[:user])
 		if @user.save 
-		flash[:success] = "Welcome to Collaborist"
+			sign_in @user
+			flash[:success] = "Welcome to Collaborist"
 			redirect_to @user
 		else
 			render 'new'
